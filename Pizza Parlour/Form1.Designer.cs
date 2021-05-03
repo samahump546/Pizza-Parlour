0f989f47323b244d642a8029de70c72305b1d063
Samantha Humphrey <samahump546@outlook.com>
2021-05-03 13:43:44 -0400

    Add project files.

---
+++ Pizza Parlour/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Pizza_Parlour
+{
+    partial class pizzaParlour
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(pizzaParlour));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toppingsInput = new System.Windows.Forms.TextBox();
+            this.priceInput = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DarkRed;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(776, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pizza Parlour";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(152, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(279, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number of Toppings";
+            // 
+            // toppingsInput
+            // 
+            this.toppingsInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toppingsInput.Location = new System.Drawing.Point(475, 132);
+            this.toppingsInput.Multiline = true;
+            this.toppingsInput.Name = "toppingsInput";
+            this.toppingsInput.Size = new System.Drawing.Size(115, 39);
+            this.toppingsInput.TabIndex = 2;
+            this.toppingsInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // priceInput
+            // 
+            this.priceInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.priceInput.Location = new System.Drawing.Point(158, 318);
+            this.priceInput.Multiline = true;
+            this.priceInput.Name = "priceInput";
+            this.priceInput.Size = new System.Drawing.Size(488, 82);
+            this.priceInput.TabIndex = 3;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.ForeColor = System.Drawing.Color.White;
+            this.calculateButton.Location = new System.Drawing.Point(298, 214);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(188, 49);
+            this.calculateButton.TabIndex = 4;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // pizzaParlour
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.priceInput);
+            this.Controls.Add(this.toppingsInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "pizzaParlour";
+            this.Text = "Pizza Parlour";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox toppingsInput;
+        private System.Windows.Forms.TextBox priceInput;
+        private System.Windows.Forms.Button calculateButton;
+    }
+}
+
---
+++ Pizza Parlour/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pizza_Parlour
+{
+    public partial class pizzaParlour : Form
+    {
+        //global varriables
+        double basePrice = 7.00;
+        double toppings = 1.25;
+        double toppingsNumber;
+        double totalPrice;
+        public pizzaParlour()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            toppingsNumber = Convert.ToDouble(toppingsInput.Text);
+            totalPrice = basePrice + toppings * toppingsNumber;
+
+            priceInput.Text = $"The price of the pizza with {toppingsNumber} toppings is: \n\n${totalPrice}";
+        }
+    }
+}
